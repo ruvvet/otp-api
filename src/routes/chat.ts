7c0aef7a69e4e82ddd5677775ba318e960e947c0
Jenny <finej01@gmail.com>
2021-01-24 23:41:47 -0500

    fix chat stuff

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -4,8 +4,8 @@ import { Chat } from '../entity/Chat';
 
 const router = Router();
 
-router.get('/', getChatConvos);
 router.get('/:buddyId', getChatHistory);
+router.get('/', getChatConvos);
 
 async function getChatHistory(req: Request, res: Response) {
   const chatRepo = getRepository(Chat);
@@ -39,6 +39,7 @@ async function getChatConvos(req: Request, res: Response) {
   const convos = await chatRepo
     .createQueryBuilder('chat')
     .leftJoin('chat.receiver', 'receiver')
+    .leftJoin('chat.sender', 'sender')
     .select([
       'chat.receiver',
       'chat.senderId',
@@ -46,18 +47,52 @@ async function getChatConvos(req: Request, res: Response) {
       'receiver.discordUsername',
       'receiver.displayName',
       'receiver.discordAvatar',
+      'sender.discordId',
+      'sender.discordUsername',
+      'sender.displayName',
+      'sender.discordAvatar',
     ])
+
     .where('chat.receiver = :id', { id: req.userId })
     .orWhere('chat.senderId = :id', { id: req.userId })
     .groupBy('chat.receiver')
     .addGroupBy('chat.senderId')
     .addGroupBy('receiver.discordId')
+    .addGroupBy('sender.discordId')
     .getRawMany();
 
+  const mergeConvos = convos.reduce((result, convo) => {
+    const { chat_senderId, receiverId } = convo;
 
+    const key =
+      chat_senderId < receiverId
+        ? `${chat_senderId}-${receiverId}`
+        : `${receiverId}-${chat_senderId}`;
 
-  //TODO: join or lookup to get additional data
-  res.json(convos);
+    if (chat_senderId === req.userId) {
+      result[key] = {
+        discordId: convo.receiver_discordId,
+        discordUsername: convo.receiver_discordUsername,
+        displayName: convo.receiver_displayName,
+        discordAvatar: convo.receiver_discordAvatar,
+      };
+    } else {
+      result[key] = {
+        discordId: convo.sender_discordId,
+        discordUsername: convo.sender_discordUsername,
+        displayName: convo.sender_displayName,
+        discordAvatar: convo.sender_discordAvatar,
+      };
+    }
+
+    return result;
+  }, {});
+
+  // [{chat_senderId: 1, receiverId: 2}, {chat_senderId: 2, receiverId: 1}]
+
+  // TODO :  FILTER/MERGE THIS
+
+  res.json(Object.values(mergeConvos));
 }
 
 module.exports = router;
